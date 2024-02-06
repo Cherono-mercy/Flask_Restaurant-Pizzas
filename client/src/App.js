8a879c5649e3a01c00f24fff8e13b38209b124c4
Cherono-mercy <cheronom142@gmail.com>
2024-02-06 16:06:39 +0800

    Updated React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import './restaurantlist.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RestaurantList from './components/RestaurantList';
-import PizzaList from './components/PizzaList';
 
-import RestaurantDetails from './components/RestaurantDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RestaurantList from './Components/RestaurantList';
+
+
+import RestaurantDetails from './Components/RestaurantDetails';
 
 function App() {
   return (
